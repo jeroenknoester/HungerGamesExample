585b34017cf6fe8116f03eb956ebb884782b03f5
Jeroen Knoester <jeroen.knoester@capgemini.com>
2018-09-18 09:36:40 +0200

    Add a GameController

---
+++ src/com/capgemini/examples/GameController.java
@@ -0,0 +1,15 @@
+package com.capgemini.examples;
+
+public class GameController {
+
+    private int day;
+
+    public GameController() {
+        this.day = 0;
+    }
+
+    public int getDay() {
+        return day;
+    }
+
+}
